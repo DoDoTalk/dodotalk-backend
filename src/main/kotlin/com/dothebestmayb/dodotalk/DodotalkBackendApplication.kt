0cbced011be283e590d35e3164c7a3f30022274f
DoTheBestMayB <send1684@gmail.com>
2025-10-14 08:04:09 +0900

    init: 프로젝트 생성

---
+++ src/main/kotlin/com/dothebestmayb/dodotalk/DodotalkBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.dothebestmayb.dodotalk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DodotalkBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DodotalkBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/dothebestmayb/dodotalk/DodotalkBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dothebestmayb.dodotalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DodotalkBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
