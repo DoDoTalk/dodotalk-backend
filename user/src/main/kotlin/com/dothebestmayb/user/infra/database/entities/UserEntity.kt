e2254ebba618341ab2f5822e286bb14760f124db
DoTheBestMayB <send1684@gmail.com>
2025-10-29 07:10:08 +0900

    feat: UserEntity 생성 및 JPA 설정

---
+++ user/src/main/kotlin/com/dothebestmayb/user/infra/database/entities/UserEntity.kt
@@ -0,0 +1,44 @@
+package com.dothebestmayb.user.infra.database.entities
+
+import com.dothebestmayb.user.domain.model.UserId
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+/**
+ * data class를 사용할 수도 있으나, toString, hashCode, equals, copy 함수는 JPA가 요구하는 것이 아니므로
+ * 일반 class를 사용함
+ */
+@Entity
+@Table(
+    name = "users",
+    schema = "user_service",
+    indexes = [
+        Index(name = "idx_users_email", columnList = "email"),
+        Index(name = "idx_users_username", columnList = "username"),
+    ]
+)
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UserId? = null,
+    @Column(nullable = false, unique = true)
+    var email: String,
+    @Column(nullable = false, unique = true)
+    var username: String,
+    @Column(nullable = false)
+    var hashedPassword: String,
+    @Column(nullable = false)
+    var hasVerifiedEmail: Boolean = false,
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now(),
+    @UpdateTimestamp
+    var updatedAt: Instant = Instant.now(),
+)
