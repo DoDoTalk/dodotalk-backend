d02eea8da5aeef9d04bd1dae78ee32dfd7e4b9e5
DoTheBestMayB <send1684@gmail.com>
2025-12-01 14:58:23 +0900

    feat: SecurityConfig를 통해 jwtAuthFilter 등록

--- user/src/main/kotlin/com/dothebestmayb/dodotalk/api/config/JwtAuthFilter.kt
+++ user/src/main/kotlin/com/dothebestmayb/dodotalk/api/config/JwtAuthFilter.kt
@@ -7,13 +7,11 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
-@Component
 class JwtAuthFilter(
     private val jwtService: JwtService,
-): OncePerRequestFilter() {
+) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
