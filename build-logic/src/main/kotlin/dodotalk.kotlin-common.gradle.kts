960d03eff95d0a520416ddf6868f2754118a2224
DoTheBestMayB <send1684@gmail.com>
2025-12-10 14:57:04 +0900

    build: build logic common plugin에 spring 의존성 추가

--- build-logic/src/main/kotlin/dodotalk.kotlin-common.gradle.kts
+++ build-logic/src/main/kotlin/dodotalk.kotlin-common.gradle.kts
@@ -1,14 +1,23 @@
+import gradle.kotlin.dsl.accessors._1a4939bdc5f6e939f50d04029c2e9574.dependencyManagement
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring")
+    id("io.spring.dependency-management")
 }
 
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:${libraries.findVersion("spring-boot").get()}")
+    }
 }
 
 configure<KotlinJvmProjectExtension> {
