b394cc64b9f29b33f984a4b1e7ab4ef04f7702a0
DoTheBestMayB <send1684@gmail.com>
2025-12-18 23:06:03 +0900

    feat: 채팅에 필요한 domain model 정의하기

---
+++ chat/src/main/kotlin/com/dothebestmayb/chat/domain/models/ChatMessage.kt
@@ -0,0 +1,13 @@
+package com.dothebestmayb.chat.domain.models
+
+import com.dothebestmayb.dodotalk.domain.type.ChatId
+import com.dothebestmayb.dodotalk.domain.type.ChatMessageId
+import java.time.Instant
+
+data class ChatMessage(
+    val id: ChatMessageId,
+    val chatId: ChatId,
+    val sender: ChatParticipant,
+    val content: String,
+    val createdAt: Instant,
+)
